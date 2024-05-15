68ff66369cb522e9592c85eba97ef11b8f0323ce
AkshatJha21 <akshatj2134@gmail.com>
2024-05-15 16:03:17 +0530

    shift operator

---
+++ Day 4/shift_operators.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "17 >> 1 " << (17 >> 1) << endl;
+    cout << "19 << 2 " << (19 << 1) << endl;
+    cout << "15 >> 1 " << (15 >> 1) << endl;
+    cout << "21 << 2 " << (21 << 2) << endl;
+}
