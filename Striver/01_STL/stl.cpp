dfb8f32aed056556adf4d9ea4506dc140ccc0e63
AkshatJha21 <akshatj2134@gmail.com>
2024-05-31 07:45:56 +0530

    vector

--- Striver/01_STL/stl.cpp
+++ Striver/01_STL/stl.cpp
@@ -8,16 +8,48 @@ void print() {
 }
 
 int main() {
-    int a;
-    cin >> a;
-    cout << a;
+    // int a;
+    // cin >> a;
+    // cout << a;
 
-    print();
+    // print();
 
-    pair<int, int> p;
-    p.first = 1;
-    p.second = 2;
-    cout << p.first << " " << p.second << endl;
+    // pair<int, int> p;
+    // p.first = 1;
+    // p.second = 2;
+    // cout << p.first << " " << p.second << endl;
+
+    vector<int> v;
+
+    v.push_back(1);
+    v.emplace_back(2);
+
+    vector<int> v1(5);
+    vector<int> v2(5, 100);
+    vector<int> v3(v2);
+
+    vector<int>::iterator it = v.begin();
+
+    it++;
+    cout << *(it) << " ";
+
+    vector<int>::iterator it = v.end();
+    // vector<int>::iterator it = v3.rend();
+    // vector<int>::iterator it = v.rbegin();
+
+    for (auto it = v.begin(); it != v.end(); it++) 
+    {
+        cout << *(it) << endl;
+    }
+
+    for (auto it : v) {
+        cout << it << " ";
+    }
+
+    v.erase(v.begin()+1);
+    cout << v.size();
+    v.clear();
+    
 
     return 0;
 }
