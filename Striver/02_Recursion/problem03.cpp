48068cd9911e7715f7e076b2a457af450ef0b2a2
AkshatJha21 <akshatj2134@gmail.com>
2024-06-01 08:24:12 +0530

    factorial using recursion

---
+++ Striver/02_Recursion/problem03.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int fact(int i) {
+    if (i == 1 || i == 0) {
+        return 1;
+    } else {
+        return i * fact(i - 1);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << fact(n) << endl;
+}
