9bdddf2d00062565e99b9f885991f89fc6fb680e
AkshatJha21 <akshatj2134@gmail.com>
2024-06-03 08:56:47 +0530

    bubble sort

---
+++ Striver/04_Sorting/bubbleSort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+// BUBBLE SORT PUSHES THE LARGEST ELEMENT TO THE END OF THE ARRAY USING ADJACENT SWAPS
+
+void bubble_sort(int arr[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        int didSwap = 0;
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    bubble_sort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
