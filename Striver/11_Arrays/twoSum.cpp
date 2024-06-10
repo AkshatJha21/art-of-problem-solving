bcb2d7a8998633ddc38423065a1b0ce041568cc9
AkshatJha21 <akshatj2134@gmail.com>
2024-06-10 09:27:41 +0530

    2 sum yes/no

--- Striver/11_Arrays/twoSum.cpp
+++ Striver/11_Arrays/twoSum.cpp
@@ -1,7 +1,40 @@
 #include<iostream>
 #include<map>
+#include<vector>
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+
+    int sum;
+    cin >> sum;
+
+    bool yes = false;
+
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        int a = arr[i];
+        int rem = sum - a;
+
+        if(mpp.find(rem) != mpp.end()) {
+            yes = true;
+        }
+        mpp[a] = i;
+    }
+    if(yes) {
+        cout << "yes" << endl;
+    } else {
+        cout << "no" << endl;
+    }
     
 }
