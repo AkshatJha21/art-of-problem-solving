551e4699c38e1c454d4cb0a652c17d6288c77372
AkshatJha21 <akshatj2134@gmail.com>
2024-06-12 09:00:12 +0530

    majority element better solution

--- Striver/13_Arrays/majorityElem.cpp
+++ Striver/13_Arrays/majorityElem.cpp
@@ -1,6 +1,33 @@
 #include<iostream>
+#include<map>
+#include<vector>
 using namespace std;
 
+int majorityElement(vector<int> arr) {
+    map<int, int> mpp;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        mpp[arr[i]]++;
+    }
+    for (auto it: mpp) {
+        if (it.second > (arr.size() / 2)) {
+            return it.first;
+        }
+    }
+    return -1;
+}
+
 int main() {
-    
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+
+    cout << majorityElement(arr) << endl;
 }
