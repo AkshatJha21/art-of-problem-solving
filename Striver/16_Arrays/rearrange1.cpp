7bf17fb0037f89430ecb73f8774244e132dd1517
AkshatJha21 <akshatj2134@gmail.com>
2024-06-15 11:27:08 +0530

    unequal members

--- Striver/16_Arrays/rearrange1.cpp
+++ Striver/16_Arrays/rearrange1.cpp
@@ -19,6 +19,45 @@ vector<int> rearrangeArray(vector<int> &nums) {
     return ans;
 }
 
+vector<int> alternateNumbers(vector<int> &arr) {
+    vector<int> pos, neg;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > 0) {
+            pos.push_back(arr[i]);
+        } else {
+            neg.push_back(arr[i]);
+        }
+    }
+
+    if (pos.size() > neg.size()) {
+        for (int i = 0; i < neg.size(); i++)
+        {
+            arr[2 * i] = pos[i];
+            arr[2 * i + 1] = neg[i];
+        }
+        int index = neg.size() * 2;
+        for (int i = neg.size(); i < pos.size(); i++)
+        {
+            arr[index] = pos[i];
+            index++;
+        }
+    } else {
+        for (int i = 0; i < pos.size(); i++)
+        {
+            arr[2 * i] = pos[i];
+            arr[2 * i + 1] = neg[i];
+        }
+        int index = pos.size() * 2;
+        for (int i = pos.size(); i < neg.size(); i++)
+        {
+            arr[index] = neg[i];
+            index++;
+        }
+    }
+    return arr;
+}
+
 int main() {
     int n;
     cin >> n;
@@ -31,7 +70,7 @@ int main() {
         arr.push_back(num);
     }
 
-    vector<int> ans = rearrangeArray(arr);
+    vector<int> ans = alternateNumbers(arr);
 
     for (int i = 0; i < n; i++)
     {
