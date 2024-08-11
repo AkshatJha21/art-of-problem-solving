1f828a089585b29c84a11509c5e74d2e1e2c5717
AkshatJha21 <akshatj2134@gmail.com>
2024-08-11 11:41:09 +0530

    pairs

---
+++ STL/pair.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    pair<int, int> p = {1, 3};
+    cout << p.first << " " << p.second << endl;
+    pair<int, pair<int, int>> pp = {1, {1, 2}};
+    pair<int, int> arr[] = {{1,2}, {2, 4}};
+}
