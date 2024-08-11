c5c6f5de62e9b8fe309c197aacdf6bf96dd47256
AkshatJha21 <akshatj2134@gmail.com>
2024-08-11 20:38:56 +0530

    ih

---
+++ STL/map.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main() {
+    map<int, int> mpp;
+    mpp[1] = 2; // {1, 2}
+    mpp.insert({3, 1}); // {3, 1}
+
+    for (auto it : mpp) {
+        cout << it.first << " " << it.second << endl;
+    }
+
+    
+}
