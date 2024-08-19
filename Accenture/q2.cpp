4a4e99ab0f390ee19cf231f93fc0f8c3ddd00ea8
AkshatJha21 <akshatj2134@gmail.com>
2024-08-20 00:49:14 +0530

    q2 solved using unordered set

--- Accenture/q2.cpp
+++ Accenture/q2.cpp
@@ -4,3 +4,31 @@
 // Input 'aaaaaaaaa' Output 'a'
 // Input 'quick fox jumps lake' Output 'quick foxjmpslae'
 
+#include<iostream>
+#include<unordered_set>
+#include<string>
+using namespace std;
+
+string removeDuplicates(const string &s) {
+    unordered_set<char> inString;
+    string ans;
+
+    for (int i = 0; i < s.length(); i++) {
+        char ch = s[i];
+        if (inString.find(ch) == inString.end()) {
+            ans += ch;
+            inString.insert(ch);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    string input;
+    cout << "Enter string: " << endl;
+    cin >> input;
+
+    string ans = removeDuplicates(input);
+    cout << ans << endl;
+    return 0;
+}
