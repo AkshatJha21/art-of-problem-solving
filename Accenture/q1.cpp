9cb11ccddf133eecb0e99ac1f87b6798ffc9ff40
AkshatJha21 <akshatj2134@gmail.com>
2024-08-19 17:45:48 +0530

    problem statements

---
+++ Accenture/q1.cpp
@@ -0,0 +1,6 @@
+// When a particular range is given to Kyle, 
+// he writes down all the values within that range
+// which consists of unique digits
+// Input 10 30
+// Output 10 12 13 14 15 16 17 18 19 20 21 23 24 25 26 27 28 29 30
+// Explanation: 11 and 22 not returned as their digits are repeated (same for 100)
---
+++ Accenture/q2.cpp
@@ -0,0 +1,5 @@
+// You have been given a string having space-separated multiple words
+// Reprint the string after removing the characters that occur more than once
+// Supposed to remove the later occurences of a character
+// Input 'aaaaaaaaa' Output 'a'
+// Input 'quick fox jumps lake' Output 'quick foxjmpslae'
