9992ef9989e792ae00ac13ff71e2d18fcb07ee50
AkshatJha21 <akshatj2134@gmail.com>
2024-08-21 16:18:38 +0530

    q8 solved

--- Accenture/q8.cpp
+++ Accenture/q8.cpp
@@ -15,3 +15,40 @@
 // Explanation :
 // The difference between values 0 and 5 is 5. 
 // If i is equal to 0 and j is equal to 5 it also satisfies j > i.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxHeightDifference(vector<int> &plants, int size) {
+    if (size < 2) return 0;
+
+    int minHeight = plants[0];
+    int maxDiff = 0;
+
+    for (int i = 1; i < size; i++) {
+        int diff = plants[i] - minHeight;
+        if (diff > maxDiff) {
+            maxDiff = max(diff, maxDiff);
+        }
+        if (plants[i] < minHeight) {
+            minHeight = plants[i];
+        }
+    }
+
+    return maxDiff;
+}
+
+int main() {
+    int size;
+    cin >> size;
+    vector<int> plants;
+    for (int i = 0; i < size; i++) {
+        int num;
+        cin >> num;
+        plants.push_back(num);
+    }
+
+    cout << maxHeightDifference(plants, size);
+    return 0;
+}
