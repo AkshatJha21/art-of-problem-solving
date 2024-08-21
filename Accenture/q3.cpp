128191bbe319fc4561e60e434289b9f7ae4f7256
AkshatJha21 <akshatj2134@gmail.com>
2024-08-21 15:06:50 +0530

    q3 solved

--- Accenture/q3.cpp
+++ Accenture/q3.cpp
@@ -8,3 +8,55 @@
 // Output: 2 4
 // Input: N = 3, S = 22, A[3] = {1,4,5}
 // Output: -1
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> subArraySum(vector<int> &arr, int n, int sum) {
+    int currentSum = 0;
+    int start = 0;
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++) {
+        currentSum += arr[i];
+
+        while (currentSum > sum && start <= i) {
+            currentSum -= arr[start];
+            start++;
+        }
+
+        if (currentSum == sum) {
+            ans.push_back(start + 1);
+            ans.push_back(i + 1);
+            break;
+        }
+    }
+
+    if (ans.size() == 0) {
+        ans.push_back(-1);
+    }
+
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int sum;
+    cin >> sum;
+    vector<int> arr;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+
+    vector<int> result = subArraySum(arr, n, sum);
+
+    for (int o = 0; o < result.size(); o++) {
+        cout << result[o] << " ";
+    }
+
+    return 0;
+}
