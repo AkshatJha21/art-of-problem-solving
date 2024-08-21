947dcfb4fe2161cf5bf1cc9213006f200a8f611e
AkshatJha21 <akshatj2134@gmail.com>
2024-08-21 15:22:42 +0530

    q5 done

--- Accenture/q5.cpp
+++ Accenture/q5.cpp
@@ -11,3 +11,35 @@
 // Explanation: horse -> ros (remove 'e')
 // Input: intention execution
 // Output: 5
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int minDHelper(string &word1, int m, string &word2, int n) {
+    if (m == 0) return n;
+    if (n == 0) return m;
+
+    if (word1[m - 1] == word2[n - 1]) {
+        return minDHelper(word1, m - 1, word2, n - 1);
+    } else {
+        int insertOp = minDHelper(word1, m, word2, n - 1);
+        int deleteOp = minDHelper(word1, m - 1, word2, n);
+        int replaceOp = minDHelper(word1, m - 1, word2, n - 1);
+
+        return 1 + min(insertOp, min(deleteOp, replaceOp));
+    }
+}
+
+int minOperations(string &word1, string &word2) {
+    return minDHelper(word1, word1.length(), word2, word2.length());
+}
+
+int main() {
+    string word1;
+    cin >> word1;
+    string word2;
+    cin >> word2;
+
+    cout << minOperations(word1, word2);
+}
