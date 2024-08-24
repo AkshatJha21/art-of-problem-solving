5ebfcef6b351a6416ad58a37bc1630c14af41c9b
AkshatJha21 <akshatj2134@gmail.com>
2024-08-24 12:10:04 +0530

    q9 problem statement

---
+++ Accenture/q9.cpp
@@ -0,0 +1,17 @@
+// You are given an array of integers
+// Your task is to find the longest subarray in which
+// elements are first sorted in increasing order and then in decreasing order
+// Your program should output the length of the bitonic subarray
+// and the elements it contains
+// Input:
+// 7
+// -4 5 6 8 9 7 -5
+// Output:
+// 7
+// -4 5 6 8 9 7 -5
+// Input:
+// 8
+// 1 3 5 2 4 6 3 2
+// Output:
+// 5
+// 2 4 6 3 2
