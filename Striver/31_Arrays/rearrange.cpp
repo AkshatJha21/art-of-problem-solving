1157bde8636aa8f3a17e0faad6d864fa3a1377ff
AkshatJha21 <akshatj2134@gmail.com>
2024-09-05 10:30:25 +0530

    rearrange array elements by sign

---
+++ Striver/31_Arrays/rearrange.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void rearrangeSigns(vector<int> &arr) {
+    int n = arr.size();
+    vector<int> ans(n, 0);
+
+    int pos = 0, neg = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < 0) {
+            ans[neg] = arr[i];
+            neg += 2;
+        } else {
+            ans[pos] = arr[i];
+            pos += 2;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+    
+    rearrangeSigns(arr);
+
+    return 0;
+}
